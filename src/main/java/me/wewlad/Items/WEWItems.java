1b49e5dec514e49921ac9a8016875da8f3eb63f2
dentron <captnstub@gmail.com>
2022-06-16 23:36:05 -0400

    bruv

--- src/main/java/me/wewlad/Items/WEWItems.java
+++ src/main/java/me/wewlad/Items/WEWItems.java
@@ -7,7 +7,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class WEWitems {
+public class WEWItems {
     public static final DeferredRegister<Item> WITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, WEWLAD.MODID);
 
     public static void register(IEventBus iebus){
