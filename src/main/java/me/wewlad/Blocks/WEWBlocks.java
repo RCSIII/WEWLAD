581a9cbd12c7447c40d4430644f13881ab31f6b6
dentron <captnstub@gmail.com>
2022-06-17 01:33:35 -0400

    tungsten!!!!!!!!!!!!!!!!!

--- src/main/java/me/wewlad/Blocks/WEWBlocks.java
+++ src/main/java/me/wewlad/Blocks/WEWBlocks.java
@@ -1,13 +1,34 @@
 package me.wewlad.Blocks;
 
+import me.wewlad.Items.WEWItems;
 import me.wewlad.WEWLAD;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
 
 public class WEWBlocks {
     public static final DeferredRegister<Block> WBLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, WEWLAD.MODID);
+
+    public static final RegistryObject<Block> TUNGSTEN_BLOCK = registerBlock("tungsten_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MATERIALS);
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String bName, Supplier<T> block, CreativeModeTab tab){
+        RegistryObject<T> ret = WBLOCKS.register(bName, block);
+        registerBlockItem(bName, ret, tab);
+        return ret;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String iName, RegistryObject<T> block, CreativeModeTab tab){
+        return WEWItems.WITEMS.register(iName, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
 
     public static void register(IEventBus iebus){
         WBLOCKS.register(iebus);
