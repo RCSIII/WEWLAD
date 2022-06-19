4ce33d1de8d599fd74dab79f56b04dfc640789b8
dentron <captnstub@gmail.com>
2022-06-19 19:18:53 -0400

    johnbo xd

--- src/main/java/me/wewlad/ExplosiveHandler/ExplosiveHandler.java
+++ src/main/java/me/wewlad/ExplosiveHandler/ExplosiveHandler.java
@@ -5,15 +5,25 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 
+import javax.annotation.Nullable;
+
 public class ExplosiveHandler {
-    public static void HandleExplosion(Entity expEntity, Level level, double x, double y, double z, ExplosiveType expType, String expModifiers){
+    public static void HandleExplosion(@Nullable Entity expEntity, Level level, double x, double y, double z, ExplosiveType expType, String expModifiers){
         switch (expType){
             case DOUBLETNT:
                 doubleTNTExplosion(expEntity, level, x, y, z);
         }
     }
 
-    private static void doubleTNTExplosion(Entity expEntity, Level level, double x, double y, double z){
-        level.explode(expEntity, x, y, z, 16.0F, Explosion.BlockInteraction.BREAK);
+    private static void doubleTNTExplosion(@Nullable Entity expEntity, Level level, double x, double y, double z){
+        level.explode(expEntity, x, y, z, 64.0F, Explosion.BlockInteraction.BREAK);
+    }
+
+    private static void fastExplode1(@Nullable Entity expEntity, Level level, double x, double y, double z, Explosion.BlockInteraction interaction){
+
+    }
+
+    private static void fastExplode2(@Nullable Entity expEntity, Level level, double x, double y, double z, Explosion.BlockInteraction interaction){
+
     }
 }
